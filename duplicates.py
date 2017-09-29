895e7e02cf408c82c1a42baef346037f7b237ccb
Evgeniy Galinov <galinove@gmail.com>
2017-09-29 11:37:54 +0500

    поиск дубликатов файлов в заданном каталоге

--- duplicates.py
+++ duplicates.py
@@ -1,44 +1,47 @@
 import os
 import sys
 
-def get_files_collection(dir,dict):
+
+def get_files_collection(dir, dict):
   for name in os.listdir(dir):
     path = os.path.join(dir, name)
     if os.path.isfile(path):
         dict[str(path)] = str(name) + ";" + str(os.path.getsize(path))
     else:
-        get_files_collection(path,dict)
+        try:
+            get_files_collection(path, dict)
+        except PermissionError:
+            print("Ошибка доступа к каталогу: ", path)
   return dict
 
+
 def print_duplicates(dir, dict):
-    try:
-        get_files_collection(dir, dict)
-    except PermissionError:
-        print("Ошибка доступа")
-    dict_unique_files=dict.fromkeys(set(dict.values()),0)
+    get_files_collection(dir, dict)
+    dict_unique_files = dict.fromkeys(set(dict.values()), 0)
     for name_size_agr, cnt_name_size in dict_unique_files.items():
         for path, name_size in dict.items():
             if name_size_agr == name_size:
-                cnt_name_size=cnt_name_size+1
+                cnt_name_size = cnt_name_size+1
         dict_unique_files[name_size_agr] = cnt_name_size
-
     for name_size_agr, cnt_name_size in dict_unique_files.items():
         if cnt_name_size > 1:
             print()
-            print("Найдены дубликаты файла " + name_size_agr.split(";")[0] + " размером " + str(int(name_size_agr.split(";")[1])//1024+1) + " Кбайт:")
+            print("Найдены дубликаты файла " + name_size_agr.split(";")[0] + " размером " + \
+                  str(int(name_size_agr.split(";")[1])//1024+1) + " Кбайт, " + str(cnt_name_size) + " штук(и):")
             for path, name_size in dict.items():
                 if name_size_agr == name_size:
                     print(path)
 
-def find_duplicates (dir):
+
+def find_duplicates(dir):
     dict = {}
-    print_duplicates(dir,dict)
+    print_duplicates(dir, dict)
+
 
 if __name__ == '__main__':
-
     if len(sys.argv) > 1:
         filepath = (sys.argv[1])
     else:
         print("Введите директорию, в которой будем искать дубли файлов (по имени и размеру)")
         filepath = input()
-    find_duplicates (filepath)
+    find_duplicates(filepath)
